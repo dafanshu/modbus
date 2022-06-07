80f2aa00ba73949dc8b347bce8086c3fbe3b712f
yurui <393615093@163.com>
2022-06-07 14:13:28 +0800

    feat:设置modbustcp也有默认间隔时间30ms

--- tcpclient.go
+++ tcpclient.go
@@ -65,8 +65,7 @@ type tcpPackager struct {
 func (mb *tcpPackager) Encode(pdu *ProtocolDataUnit) (adu []byte, err error) {
 	adu = make([]byte, tcpHeaderSize+1+len(pdu.Data))
 
-	// Transaction identifier todo 暂时将这里改成0，后续有问题再回归
-	transactionId := atomic.AddUint32(&mb.transactionId, 0)
+	transactionId := atomic.AddUint32(&mb.transactionId, 1)
 	binary.BigEndian.PutUint16(adu, uint16(transactionId))
 	// Protocol identifier
 	binary.BigEndian.PutUint16(adu[2:], tcpProtocolIdentifier)
@@ -172,6 +171,7 @@ func (mb *tcpTransporter) Send(aduRequest []byte) (aduResponse []byte, err error
                 }
 		return
 	}
+	time.Sleep(time.Duration(30) * time.Millisecond)
 	// Read header first
 	var data [tcpMaxLength]byte
 	if _, err = io.ReadFull(mb.conn, data[:tcpHeaderSize]); err != nil {
